2dc628a1fc1f0c36dbe617fe8eefd98be9008671
Tony Alvarez <tonyalva87@gmail.com>
2022-07-16 16:56:44 -0700

    update readme

--- src/Board.jsx
+++ src/Board.jsx
@@ -35,7 +35,7 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows = 3, ncols = 3, chanceLightStartsOn = 0.5 }) {
+function Board({ nrows = 3, ncols = 3, chanceLightStartsOn = 0.25 }) {
   const [board, setBoard] = useState(createBoard());
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
